f2cf937cc37131e3dd27fd99a679e94389e53f65
farism9q <faresm9q@gmail.com>
2024-10-28 07:56:23 +0300

    Fix an error in "edit-user-profile-sheet" file

--- features/account/components/edit-user-profile-sheet.tsx
+++ features/account/components/edit-user-profile-sheet.tsx
@@ -62,7 +62,7 @@ const formSchema = z
 export const EditUserProfileSheet = () => {
   const { data, isOpen, onClose } = useEditProfileSheet();
   const { user } = data;
-  const { mutate, isLoading: isPending } = useEditAccount();
+  const { mutate, isPending } = useEditAccount();
 
   const [avatar, setAvatar] = useState("");
   const [avatarKey, setAvatarKey] = useState("");
